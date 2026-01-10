ff0788cc8e6a86193cf7f7a49753d55d4a5f0ff0
TR114514 <tr114514-mail@qq.com>
2026-01-10 20:23:23 +0800

    Create GmeekFont.js

---
+++ assets/GmeekFont.js
@@ -0,0 +1,31 @@
+// 定义字体URL和字体名称
+const fontUrl = 'https://blog-assets.traveler.dpdns.org/font/MiSans-Heavy.ttf';
+const fontName = 'CustomFont';
+
+// 创建字体face规则
+const fontFace = `
+@font-face {
+  font-family: '${fontName}';
+  src: url('${fontUrl}');
+  font-display: swap;
+}
+`;
+
+// 创建样式元素
+const style = document.createElement('style');
+style.textContent = fontFace;
+document.head.appendChild(style);
+
+// 字体加载成功后应用
+document.fonts.load(`16px ${fontName}`).then(() => {
+  const applyStyle = document.createElement('style');
+  applyStyle.textContent = `
+    body, body * {
+      font-family: '${fontName}', inherit !important;
+    }
+  `;
+  document.head.appendChild(applyStyle);
+  console.log('自定义字体已应用');
+}).catch(() => {
+  console.log('使用系统默认字体');
+});
