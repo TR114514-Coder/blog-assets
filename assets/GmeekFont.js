cb6fc4a00c41d2e0e990a7b3f9f022baa69fd405
TR114514 <tr114514-mail@qq.com>
2026-01-10 20:45:11 +0800

    Update GmeekFont.js

--- assets/GmeekFont.js
+++ assets/GmeekFont.js
@@ -1,7 +1,7 @@
 // 简洁版字体应用脚本
 (function() {
   // 字体URL和名称
-  const fontUrl = 'https://blog-assets.traveler.dpdns.org/font/MiSans-Heavy.ttf';
+  const fontUrl = 'https://blog-assets.traveler.dpdns.org/font/HarmonyOS_Sans_TC_Black.ttf';
   const fontName = 'CustomFont';
   
   // 创建字体定义
