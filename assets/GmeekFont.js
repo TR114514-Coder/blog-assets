7f490c90b1edea841a1a2ab64628ad65c216d5c1
TR114514 <tr114514-mail@qq.com>
2026-01-11 08:20:34 +0800

    Create GmeekFont.js

---
+++ assets/GmeekFont.js
@@ -0,0 +1,18 @@
+// 在开头定义字体URL
+const FONT_URL = 'https://blog-assets.traveler.dpdns.org/font/KFOMCnqEu92Fr1ME7kSn66aGLdTylUAMQXC89YmC2DPNWuZtammTggvWl0Qn.ttf';
+
+// 应用字体到所有元素
+const fontStyle = document.createElement('style');
+fontStyle.textContent = `
+  @font-face {
+    font-family: 'Font113332';
+    src: url('${FONT_URL}') format('truetype');
+  }
+  
+  * {
+    font-family: 'GlobalFont', sans-serif !important;
+  }
+`;
+
+// 添加到文档头部
+document.head.appendChild(fontStyle);
