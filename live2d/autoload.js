4677b57b6f0834c5910496e52b3681bb7b396e2d
TR114514 <tr114514-mail@qq.com>
2026-01-11 10:03:54 +0800

    Update autoload.js

--- live2d/autoload.js
+++ live2d/autoload.js
@@ -59,7 +59,7 @@ function loadExternalResource(url, type) {
     cdnPath: 'https://blog-assets.traveler.dpdns.org/live2d_api/',
     cubism2Path: live2d_path + 'live2d.min.js',
     cubism5Path: 'https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js',
-    tools: ['hitokoto', 'asteroids', 'switch-model', 'switch-texture', 'photo', 'info', 'quit'],
+    tools: ['hitokoto', 'asteroids', 'switch-model', 'switch-texture', 'photo', 'info'],
     logLevel: 'warn',
     drag: true,
   });
